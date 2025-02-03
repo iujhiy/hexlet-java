39e598d2fa53e3198f9a1beae90768e0b910ebcd
Nikita <nikitazerkalev@gmail.com>
2025-02-03 18:24:15 +0300

    change One.java

--- One.java
+++ One.java
@@ -1,6 +1,10 @@
 package home.uzurpator;
+
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+	var reverseWord = StringUtils.reverse("Hexlet");
+        System.out.println(reverseWord);
     }
 }
